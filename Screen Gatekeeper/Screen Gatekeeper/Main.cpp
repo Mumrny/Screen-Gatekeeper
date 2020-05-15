011ac7771f1e22051df732594583acc5b648c79d
tonki <tonkiHouse>
2020-05-15 15:10:18 +0900

    コールバック関数だけ自前のものに出来る設定を追加

--- Screen Gatekeeper/Screen Gatekeeper/Main.cpp
+++ Screen Gatekeeper/Screen Gatekeeper/Main.cpp
@@ -1,30 +1,68 @@
 #include <windows.h>
 #include <DxLib.h>
+#include <cassert>
+
+WNDPROC dxWindowProcedure;
+HWND hMainWnd;
+
+struct Pos {
+	int x;
+	int y;
+};
+
+Pos rcPos = { 700 , 300 };
+
+void Draw(void) {
+	WINDOWINFO windowInfo;
+	windowInfo.cbSize = sizeof(WINDOWINFO);
+	GetWindowInfo(hMainWnd, &windowInfo);
+
+	ClsDrawScreen();
+	DrawBox(
+		rcPos.x - windowInfo.rcClient.left,
+		rcPos.y - windowInfo.rcClient.top,
+		rcPos.x + 100 - windowInfo.rcClient.left,
+		rcPos.y + 100 - windowInfo.rcClient.top,
+		0xffffff,
+		true
+	);
+
+	DrawFormatString(0, 0, 0xffffff, "%d, %d", windowInfo.rcClient.left, windowInfo.rcClient.top);
+	ScreenFlip();
+}
+
+LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+	switch (msg) {
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	case WM_MOVE:
+		//Draw(); 使うと止まる(処理が重い？)
+		break;
+	}
+	
+	return CallWindowProc(dxWindowProcedure, hwnd, msg, wparam, lparam);
+}
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	SetWindowText("Screen Gatekeeper");
 	SetGraphMode(800, 600, 16);
 	ChangeWindowMode(true);
+
 	if (DxLib_Init() == -1)
 	{
 		return -1; // DxLib_Initの失敗により終了
 	}
+
 	SetDrawScreen(DX_SCREEN_BACK);
 
-	HWND hDesktopWnd = GetDesktopWindow();
-	HWND hMainWnd = GetMainWindowHandle();
+	// DxLibのウィンドウ+自前のコールバック関数にするための設定
+	hMainWnd = GetMainWindowHandle();
+	dxWindowProcedure = (WNDPROC)GetWindowLongPtr(hMainWnd, GWLP_WNDPROC); // GWLP_WNDPROC(ウィンドウプロシージャのアドレス)
+	SetWindowLongPtr(hMainWnd, GWLP_WNDPROC, (LONG_PTR)WindowProcedure);
 
-	WINDOWINFO windowInfo;
-	windowInfo.cbSize = sizeof(WINDOWINFO);
-
-	struct Pos {
-		int x;
-		int y;
-	};
-
-	Pos rcPos = { 700 , 300 };
-	Pos rcWindowCorrection = { 8 , 31 };
+	/*HWND hDesktopWnd = GetDesktopWindow();*/
 
 	char key[256];
 
@@ -45,31 +83,24 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 			rcPos.y -= 3;
 		}
 
-		InvalidateRect(hDesktopWnd, nullptr, true);
-		UpdateWindow(hDesktopWnd);
+		/*InvalidateRect(hDesktopWnd, nullptr, true);
+		UpdateWindow(hDesktopWnd);*/
 
+		WINDOWINFO windowInfo;
 		GetWindowInfo(hMainWnd, &windowInfo);
 
-		ClsDrawScreen();
-		DrawBox(
-			rcPos.x - windowInfo.rcWindow.left - rcWindowCorrection.x,
-			rcPos.y - windowInfo.rcWindow.top - rcWindowCorrection.y,
-			rcPos.x + 100 - windowInfo.rcWindow.left - rcWindowCorrection.x,
-			rcPos.y + 100 - windowInfo.rcWindow.top - rcWindowCorrection.y,
-			0xffffff,
-			true
-		);
+		Draw();
 
-		DrawFormatString(0, 0, 0xffffff, "%d, %d", windowInfo.rcWindow.left, windowInfo.rcWindow.top);
-		ScreenFlip();
+		//HDC hdc;
+		//hdc = CreateDC("DISPLAY", nullptr, nullptr, nullptr);
+		//SelectObject(hdc, GetStockObject(WHITE_BRUSH));
+		////SelectObject(hdc, CreateSolidBrush(RGB(0xFF, 0, 0)));
+		//Rectangle(hdc, rcPos.x, rcPos.y, rcPos.x + 100, rcPos.y + 100);
+		//DeleteObject(SelectObject(hdc, GetStockObject(WHITE_BRUSH)));
+		//DeleteDC(hdc);
 
-		HDC hdc;
-		hdc = CreateDC("DISPLAY", nullptr, nullptr, nullptr);
-		SelectObject(hdc, GetStockObject(WHITE_BRUSH));
-		//SelectObject(hdc, CreateSolidBrush(RGB(0xFF, 0, 0)));
-		Rectangle(hdc, rcPos.x, rcPos.y, rcPos.x + 100, rcPos.y + 100);
-		DeleteObject(SelectObject(hdc, GetStockObject(WHITE_BRUSH)));
-		DeleteDC(hdc);
+		/*InvalidateRect(hDesktopWnd, nullptr, true);
+		UpdateWindow(hDesktopWnd);*/
 	}
 
 	DxLib_End();
