1284b2a63eb7f21d1dc374663063bcade5bc9dd1
tonki <tonkiHouse>
2020-05-25 09:20:21 +0900

    タスクバーの表示を実装

--- Screen Gatekeeper/Screen Gatekeeper/Main.cpp
+++ Screen Gatekeeper/Screen Gatekeeper/Main.cpp
@@ -70,18 +70,76 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	// (恐らく第一引数が解釈に使うためのﾋﾞｯﾄﾏｯﾌﾟの情報、第二引数が実ﾃﾞｰﾀ)
 	int imageDesktop = CreateGraphFromBmp(&bmpInfoDesktop, pData);
 
+	// ﾀｽｸﾊﾞｰの表示
+	HWND hTaskbarWnd = FindWindow("Shell_TrayWnd", nullptr);
+	
+	RECT rcTaskbar;
+	GetWindowRect(hTaskbarWnd, &rcTaskbar);
+	const int widthTaskbar = rcTaskbar.right;
+	const int heightTaskbar = rcTaskbar.bottom - rcTaskbar.top;
+
+	BITMAPINFO bmpInfoTaskbar;
+	bmpInfoTaskbar.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+	bmpInfoTaskbar.bmiHeader.biWidth = widthTaskbar;
+	bmpInfoTaskbar.bmiHeader.biHeight = heightTaskbar;
+	bmpInfoTaskbar.bmiHeader.biPlanes = 1;
+	bmpInfoTaskbar.bmiHeader.biBitCount = 32;
+	bmpInfoTaskbar.bmiHeader.biCompression = BI_RGB;
+
+	hdc = GetDC(hMainWnd);
+	LPDWORD lpPixelTaskbar;
+	HBITMAP hBitmapTaskbar = CreateDIBSection(hdc, &bmpInfoTaskbar, DIB_RGB_COLORS, (void**)&lpPixelTaskbar, nullptr, 0);
+	HDC hMemDcTaskbar = CreateCompatibleDC(hdc);
+	ReleaseDC(hMainWnd, hdc);
+
+	SelectObject(hMemDcTaskbar, hBitmapTaskbar);
+	PrintWindow(hTaskbarWnd, hMemDcTaskbar, 0);
+
+	BITMAP DDBInfoTaskbar;
+	GetObject(hBitmapTaskbar, sizeof(BITMAP), &DDBInfoTaskbar);
+	BYTE *pDataTaskbar = new BYTE[DDBInfoTaskbar.bmWidth * DDBInfoTaskbar.bmHeight * 4];
+
+	BITMAPINFO DIBInfoTaskbar;
+	DIBInfoTaskbar.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+	DIBInfoTaskbar.bmiHeader.biWidth = DDBInfoTaskbar.bmWidth;
+	DIBInfoTaskbar.bmiHeader.biHeight = DDBInfoTaskbar.bmHeight;
+	DIBInfoTaskbar.bmiHeader.biPlanes = 1;
+	DIBInfoTaskbar.bmiHeader.biBitCount = 32;
+	DIBInfoTaskbar.bmiHeader.biCompression = BI_RGB;
+
+	hdc = GetDC(hMainWnd);
+	GetDIBits(hdc, hBitmapTaskbar, 0, DDBInfoTaskbar.bmHeight, (void*)pDataTaskbar, &DIBInfoTaskbar, DIB_RGB_COLORS);
+	ReleaseDC(hMainWnd, hdc);
+
+	int imageTaskbar = CreateGraphFromBmp(&bmpInfoTaskbar, pDataTaskbar);
+
 	while ((ProcessMessage() == 0) && (CheckHitKey(KEY_INPUT_ESCAPE) == 0)) {
 		ClsDrawScreen();
 
 		DrawGraph(0, 0, imageDesktop, false);
+		DrawGraph(0, rcTaskbar.top, imageTaskbar, false);
 
 		ScreenFlip();
+
+		// タスクバーの情報を更新
+		SelectObject(hMemDcTaskbar, hBitmapTaskbar);
+		PrintWindow(hTaskbarWnd, hMemDcTaskbar, 0);
+
+		hdc = GetDC(hMainWnd);
+		GetDIBits(hdc, hBitmapTaskbar, 0, DDBInfoTaskbar.bmHeight, (void*)pDataTaskbar, &DIBInfoTaskbar, DIB_RGB_COLORS);
+		ReleaseDC(hMainWnd, hdc);
+
+		ReCreateGraphFromBmp(&bmpInfoTaskbar, pDataTaskbar, imageTaskbar);
 	}
 
 	// 自分で用意した分の後処理
 	delete pData;
 	DeleteDC(hMemDC);
 	DeleteObject(hBitmapDesktop);
+
+	delete pDataTaskbar;
+	DeleteDC(hMemDcTaskbar);
+	DeleteObject(hBitmapTaskbar);
 
 	DxLib_End();
 	return 0;
